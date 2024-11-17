88fa3e8523d1e3696d1fa877db75049b175d3006
yigitnurver <yigitnurver643@gmail.com>
2024-11-17 10:39:06 -0500

    Basic Structure of solar system

---
+++ src/test/java/BasicStructure.java
@@ -0,0 +1,5 @@
+public class BasicStructure {
+    String sun;
+    String moon;
+    String planet;
+}
---
+++ src/test/java/SolarSystem.java
@@ -0,0 +1,8 @@
+public class SolarSystem {
+    public static void main(String[] args) {
+        BasicStructure solar=new BasicStructure();
+        solar.sun="1";
+        solar.moon="1";
+        solar.planet="2";
+    }
+}
